01290b3d1c420e91231c155ce5b747e0dd605b93
stephen fortune <steve.fortune@icecb.com>
2015-06-24 13:27:38 +0100

    Moved user creation to a material dialog

--- client/core/app.js
+++ client/core/app.js
@@ -16,14 +16,30 @@ if (Meteor.isCordova) {
     angular.element(document).ready(boot);
 }
 
-app.controller('AddressBookCtrl', ['$scope', '$meteor', '$meteorUtils', function($scope, $meteor, $meteorUtils) {
+app.controller('NewUserCtrl', ['$mdDialog', '$scope', function($mdDialog, $scope) {
 
+    $scope.newUser = {};
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+
+}]);
+
+app.controller('AddressBookCtrl', ['$scope', '$meteor', '$meteorUtils', '$mdDialog', function($scope, $meteor, $meteorUtils, $mdDialog) {
+
+    $scope.selectStates = [ 'Users', 'Contacts', 'In call tree', 'Not in call tree' ];
     $scope.newUser = {};
     $scope.users = $meteor.collection(Meteor.users).subscribe('users');
 
     $scope.addUser = function() {
-        $scope.users.save($scope.newUser);
-        $scope.newUser = {};
+        $mdDialog.show({
+            templateUrl: 'client/newUser.ng.html',
+            controller: 'NewUserCtrl',
+            onComplete: function(scope) {
+                $scope.users.save(scope.newUser);
+                $scope.newUser = {};
+            }
+        });
     };
 
 }]);
