640074168afea5f25ddb15dcbd4b2dd8532d4cf4
stephen fortune <steve.fortune@icecb.com>
2015-06-24 10:33:53 +0100

    Simplified setup a bit

--- client/core/app.js
+++ client/core/app.js
@@ -1,7 +1,29 @@
 'use strict';
 
 var app = angular.module('isa', [
-    'isa.addressbook',
+    'angular-meteor',
     'ui.router',
     'ngMaterial'
 ]);
+
+var boot = function() {
+    angular.bootstrap(document, [ 'addressbook' ]);
+};
+
+if (Meteor.isCordova) {
+    angular.element(document).on("deviceready", boot);
+} else {
+    angular.element(document).ready(boot);
+}
+
+app.controller('AddressBookCtrl', function() {
+
+});
+
+app.config('$stateProvider', function($stateProvider) {
+    $stateProvider.state('addressbook', {
+        url : '/addressbook/:userId',
+        templateUrl: '/client/address-book.html',
+        controller: 'AddressBookCtrl'
+    });
+});
